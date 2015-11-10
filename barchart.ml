266c4d8ce736902e793b800275ba3b9f28718584
Vinaya Mandke <vinayamandke@gmail.com>
2015-11-10 07:59:52 +0530

    More additional changes

--- barchart.ml
+++ barchart.ml
@@ -9,7 +9,7 @@ module Html = Dom_html
 let (>>=) = Lwt.bind
 
 let getData () =
-	let arrayData = [1;33;23;78;79;67;87;35;28;90;207;76;45;36;88;87;45;2;89;77;98;56;46;89]
+	let arrayData = [1;33;23;78;79;67;87;35;28;90;207;76;45;36;88;87;45;10;89;77;98;56;46;89;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45;79;67;87;35;28;90;207;76;45;36;88;87;45]
 		in
 	Lwt.return (List.map float_of_int arrayData)
 
@@ -48,6 +48,9 @@ let away_sd = fun sd mean value ->
 
 let barPlotWidth = 100.
 let barPlotMargin = 10.
+let dragWidth = ref 0.
+let need_redraw = ref false
+let redraw_funct = ref (fun () -> ())
 
 let barPlot = fun canvas2Dcontext canvasHeight xStart y ->
 	Firebug.console##log(Js.string "plot a bar");
@@ -56,6 +59,23 @@ let barPlot = fun canvas2Dcontext canvasHeight xStart y ->
 
 let getStartIndex = fun dragWidth ->
 	int_of_float(dragWidth /. (barPlotWidth +. barPlotMargin))
+
+let getMaxDragWidth = fun len canvasWidth ->
+	(float (len) *. (barPlotWidth +. barPlotMargin)) -. canvasWidth
+
+
+let perform_redraw () =
+  need_redraw := false;
+  !redraw_funct ()
+
+let schedule_redraw now =
+  if not !need_redraw then begin
+    need_redraw := true;
+    Html._requestAnimationFrame
+      (Js.wrap_callback (fun () ->
+        if !need_redraw then perform_redraw ()))
+  end
+
 
 let plot = fun canvas data gridSlots sd startFromY dragWidth->
 	(*Start from denotes where we start from on Y axis*)
@@ -77,11 +97,81 @@ let plot = fun canvas data gridSlots sd startFromY dragWidth->
   			in
   				let startIndex = getStartIndex dragWidth in
 	  				plotter (0. -. dragWidth +. (float(startIndex) *. (barPlotWidth +. barPlotMargin))) startIndex;
-
     ()
+
+let markGridY = fun yGridCanvas gridSlots standard_deviation ->
+	Firebug.console##log(Js.string "marking grid");
+	let c = yGridCanvas##getContext (Html._2d_) in
+	let w = float yGridCanvas##width in
+  	let h = float yGridCanvas##height in
+  		c##clearRect (0., 0., w, h);
+  		c##font <- Js.string "12pt Verdana";
+  		let rec markSlots = fun i ->
+  			if i <= (gridSlots) then begin
+	  			c##fillStyle <- Js.string "darkkhaki";
+	  			c##fillRect ( (w *. 0.5), ( h -. (float(gridSlots - i) *. (h /. float(gridSlots))) -. 1.), (w *. 0.8), 1.);
+	  			c##fillStyle <- Js.string "black";
+	  			c##fillText ( Js.string (Printf.sprintf "%.2f" (float(gridSlots - i) *. standard_deviation)), (w *. 0.1), ( h -. (float(gridSlots - i) *. (h /. float(gridSlots))) -. 1.));
+	  			markSlots (i+1)
+  			end else 
+	  			() 
+	  		in
+	  			markSlots 0;
+	()
+
+let markGridLines = fun gridLines gridSlots standard_deviation ->
+	Firebug.console##log(Js.string "marking gridLines");
+	let c = gridLines##getContext (Html._2d_) in
+	let w = float gridLines##width in
+  	let h = float gridLines##height in
+  		c##clearRect (0., 0., w, h);
+  		let rec markSlotsX = fun i ->
+  			if i <= (gridSlots) then begin
+	  			c##fillStyle <- Js.string "darkkhaki";
+	  			c##fillRect ( (0.), ( h -. (float(gridSlots - i) *. (h /. float(gridSlots))) -. 1.), w, 1.);
+	  			markSlotsX (i+1)
+  			end else 
+	  			() 
+	  		in
+	  			markSlotsX 0;
+	()
 
 
 let data = getData ()
+
+let handle_drag = fun element move (* stop click *) ->
+	let fuzz = 4 in
+		element##onmousedown <- Html.handler (fun ev ->
+       		let x0 = ev##clientX in
+       			let started = ref false in
+       			let c1 = Html.addEventListener Html.document Html.Event.mousemove
+       				(Html.handler  (fun ev ->
+                 		let x = ev##clientX in
+                 			if not !started && (abs (x - x0) > fuzz) then begin
+                 				started := true;
+                 				element##style##cursor <- Js.string "move";
+                 			end;
+                 			if !started then move x0 x;
+	                 		Html.stopPropagation ev;
+	                 		Js._true
+                 	)) Js._true (* mousemove handler *)
+       			in
+	       			let c2 = ref Js.null in
+	      				c2 := Js.some (Html.addEventListener Html.document Html.Event.mouseup
+	      					(Html.handler  (fun ev ->
+	      						Html.removeEventListener c1;
+	      						if !started then begin
+	      							element##style##cursor <- Js.string "";
+	      							started := false;
+	      							need_redraw := false;
+	      							Js._true (* stop ev##clientX; *)
+	      						end
+	      						else 
+	      							(* click ev##clientX; *)
+	      							Js._true
+	      					))(* mouseup handler *)
+	      				Js._true);
+		Js._true)
 
 let start _ =
   Lwt.ignore_result
@@ -95,10 +185,25 @@ let start _ =
 			    doc##body##style##overflow <- Js.string "hidden";
 			    doc##body##style##margin <- Js.string "0px";
 			    doc##body##style##height <- Js.string "100%";
+			    dragWidth := 0.;
+			    redraw_funct := (fun () -> ());
 			    let w = page##clientWidth in
 			     	let h = (page##clientHeight / 2) in
+			     	let holder = Html.createDiv Html.document in
+			     		holder##style##display <- Js.string "flex";
 			     		let canvas = create_canvas w h in
-			    			 Dom.appendChild doc##body canvas;
+			     		let gridLines = create_canvas w h in
+			     			 canvas##style##display <- Js.string "block";
+			    			 Dom.appendChild doc##body holder;
+			    			 let yGridcanvas = create_canvas (page##clientWidth / 10) (page##clientHeight / 2) in
+								Dom.appendChild holder yGridcanvas;
+							 let plotholder = Html.createDiv Html.document in
+							 plotholder##style##position <- Js.string "relative";
+							 canvas##style##position <- Js.string "absolute";
+							 gridLines##style##position <- Js.string "absolute";
+			    			 Dom.appendChild holder plotholder;
+			    			 Dom.appendChild plotholder gridLines;
+			    			 Dom.appendChild plotholder canvas;
 			    			 let standard_deviation = getSD arrayData in 
 							 let avg = getAvg arrayData in
 							 let maxVal = max arrayData in
@@ -111,7 +216,27 @@ let start _ =
 									*)
 									let gridSlots = sdAwayMin + sdAwayMax in
 									let startFromY = (avg -. (standard_deviation *. float(sdAwayMin))) in
-										plot canvas arrayData gridSlots standard_deviation startFromY 0.;
+										redraw_funct := (fun () ->
+											need_redraw := false;
+											let w = page##clientWidth in
+											let h = page##clientHeight / 2 in
+												canvas##width <- w;
+												canvas##height <- h;
+												plot canvas arrayData gridSlots standard_deviation startFromY !dragWidth;
+										);
+										perform_redraw ();
+
+										markGridY yGridcanvas gridSlots standard_deviation;
+										markGridLines gridLines gridSlots standard_deviation;
+
+							handle_drag canvas (fun x0 x1 -> 
+								dragWidth := !dragWidth +. float(x0 - x1);
+								if !dragWidth < 0. then dragWidth := 0.;
+								let maxDragWidth = getMaxDragWidth (List.length arrayData) (float w) in
+									if !dragWidth > maxDragWidth then dragWidth := maxDragWidth;
+								schedule_redraw true
+							);
+
       Lwt.return () (* Lwt.return creates a thread which has aldready terminated*)
    );
    Js._false
